09af64dd63e8745b2206973ab1bc99ad9214eef6
Unknown <Bomber.qw@gmail.com>
2018-03-15 13:57:32 +0200

    Add Edit and delete garages

--- app/controllers/admin/garages_controller.rb
+++ app/controllers/admin/garages_controller.rb
@@ -6,7 +6,7 @@ module Admin
         'users.surname, users.name, garages.garage_name, garages.description,
         garages.city, garages.address, garages.phone, garages.diler,
         garages.image, garages.garage_rating, garages.garage_types,
-        garages.work_shedule'
+        garages.work_shedule, garages.id'
       )
     end
 
@@ -17,12 +17,31 @@ module Admin
       redirect_to admin_garages_path
     end
 
+    def edit
+      @garage = Garage.find(params[:id])
+    end
+
+    def update
+      garage = Garage.find(params[:id])
+      if garage.update(garage_params)
+        redirect_to admin_garages_path
+      else
+        render action 'edit'
+      end
+    end
+
+    def destroy
+      garage = Garage.find(params[:id])
+      garage.destroy
+      redirect_to admin_garages_path
+    end
+
     private
 
     def garage_params
       params.require(:garage).permit(
-        :user_id, :garage_name, :city, :address, :description,
-        :phone, :diler, :image_url, work_shedule: [], garage_types: []
+        :user_id, :garage_name, :city, :address, :description, :phone, :diler,
+        :image_url, work_shedule: [], garage_types: []
       )
     end
   end
