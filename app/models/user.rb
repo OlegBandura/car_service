4c3e2dc018416dd6962909e113726dea57e2bfec
Unknown <Bomber.qw@gmail.com>
2018-03-15 23:45:16 +0200

    Add roles to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,24 @@
+# User mogel
 class User < ApplicationRecord
   has_many :garages, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  ROLES = %i[admin user moderator director]
+  def roles=(roles)
+    roles = [*roles].map { |r| r.to_sym }
+    self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.inject(0, :+)
+  end
+
+  def roles
+    ROLES.reject do |r|
+      ((roles_mask.to_i || 0) & 2**ROLES.index(r)).zero?
+    end
+  end
+
+  def has_role?(role)
+    roles.include?(role)
+  end
 end
