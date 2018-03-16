93d07948b4aa0a242b424a3681f4b793885f11b0
Unknown <Bomber.qw@gmail.com>
2018-03-16 14:08:19 +0200

    Add role admin to users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,18 +11,17 @@ module Admin
 
     def update
       user = User.find(params[:id])
-      # if user.update(user_params)
-      #   redirect_to admin_users_path
-      # else
-      #   render action: 'edit'
-      # end
-      render plain: user_params
+      if user.update(user_params)
+        redirect_to admin_users_path
+      else
+        render action: 'edit'
+      end
     end
 
     private
 
     def user_params
-      params.require(:user).permit(:roles_mask)
+      params.require(:user).permit(:admin)
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   ROLES = %i[client director]
+  ADMINS = %i[admin manager]
   def roles=(roles)
     roles = [*roles].map { |r| r.to_sym }
     self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.inject(0, :+)
