127a6bcb99e8df97e66f45d130e9a0343ab3ae5c
Unknown <Bomber.qw@gmail.com>
2018-03-19 23:56:04 +0200

    Add migration add_roles_to_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,19 +6,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  ROLES = %i[client director]
-  def roles=(roles)
-    roles = [*roles].map { |r| r.to_sym }
-    self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.inject(0, :+)
-  end
+  # ROLES = %i[client director]
+  # def roles=(roles)
+  #   roles = [*roles].map { |r| r.to_sym }
+  #   self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.inject(0, :+)
+  # end
+  #
+  # def roles
+  #   ROLES.reject do |r|
+  #     ((roles_mask.to_i || 0) & 2**ROLES.index(r)).zero?
+  #   end
+  # end
+  #
+  # def has_role?(role)
+  #   roles.include?(role)
+  # end
 
-  def roles
-    ROLES.reject do |r|
-      ((roles_mask.to_i || 0) & 2**ROLES.index(r)).zero?
-    end
-  end
+  extend Enumerize
 
-  def has_role?(role)
-    roles.include?(role)
-  end
+  serialize :user_roles, Array
+  enumerize :user_roles, in:
+  {
+    'Клієнт' => ':client', 'Власник СТО' => ':director'
+  }, multiple: true
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180316115239) do
+ActiveRecord::Schema.define(version: 20180319215207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,6 +118,7 @@ ActiveRecord::Schema.define(version: 20180316115239) do
     t.datetime "updated_at", null: false
     t.integer "roles_mask"
     t.boolean "admin"
+    t.string "roles", default: [], array: true
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
