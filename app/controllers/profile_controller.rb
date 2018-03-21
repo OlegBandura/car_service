8c2df88cc215bd9fe8fd61707b8d97c8e5e9a266
Unknown <Bomber.qw@gmail.com>
2018-03-21 15:32:38 +0200

    Add controller orders

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -3,19 +3,22 @@ class ProfileController < ApplicationController
   def index
     @user_id = current_user.id
     @user_car = UserCar.joins(:car_model, :brand).select(
-      'car_models.model, user_cars.car_year, brands.brand_name'
+      'car_models.model, user_cars.car_year, brands.brand_name, user_cars.id'
     ).where('user_id = ? and brands.id = user_cars.brand_id', @user_id)
-
-
-
     @year = Array[]
     Time.current.year.downto(1970) { |n| @year.push(n) }
-
   end
 
   def create
     user_car = UserCar.new(profile_params)
     user_car.save
+    redirect_to profile_index_path
+  end
+
+  def destroy
+    user_car = UserCar.find(params[:id])
+    user_car.destroy
+    redirect_to profile_index_path
   end
 
   private
