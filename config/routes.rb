647991c0d77bc2e1711290399ebcd35694eec907
Unknown <Bomber.qw@gmail.com>
2018-03-28 11:26:30 +0300

    Add gem bullet

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount RailsAdmin::Engine => '/administrator', as: 'rails_admin'
   devise_for :users
   root to: 'home#index'
   get 'home/index'
