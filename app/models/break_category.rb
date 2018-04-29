bcb711f5e66732ae5fb8a32c0045f64390275d80
Unknown <Bomber.qw@gmail.com>
2018-04-29 18:08:12 +0300

    Fix admin panel break destroys

--- app/models/break_category.rb
+++ app/models/break_category.rb
@@ -1,7 +1,7 @@
 # breakCategory model
 class BreakCategory < ApplicationRecord
   has_many :break_sub_categories, dependent: :destroy
-  belongs_to :garage_break_category
+  # belongs_to :garage_break_category
 
   #
   # scope :break_sub_category, -> { joins(:break_sub_categories).select(
@@ -14,11 +14,10 @@ class BreakCategory < ApplicationRecord
   #   break_sub_categories.break_category_id") }
 
   scope :select_category_sub_category, -> {
-    left_joins(:break_sub_categories).select(
-    'break_categories.break_category_name,
+    left_joins(:break_sub_categories).select('break_categories.break_category_name,
     break_sub_categories.break_sub_category_name,
-    break_sub_categories.price'
-    )
+    break_sub_categories.price, break_sub_categories.id,
+    break_sub_categories.break_category_id')
   }
 
 end
