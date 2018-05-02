8f26f3b1d0b05f854e7bac8215ff73bd9978952f
Unknown <Bomber.qw@gmail.com>
2018-05-02 23:19:34 +0300

    Work with add destroy to garage

--- app/controllers/admin/garage_break_categories_controller.rb
+++ app/controllers/admin/garage_break_categories_controller.rb
@@ -1,14 +1,24 @@
 module Admin
   # garage break category
   class GarageBreakCategoriesController < BaseController
-    def create
 
+    def index; end
+
+    def new
+      @break_categories = BreakCategory.select_category
+    end
+
+    def create
+      # @garage_id = Garage.find(params[:id])
+      # render plain: @garage_id
+      render plain: :garage_break_category
+      # @garage_break_category = GarageBreakCategory.create(break_category_params)
     end
 
     private
 
     def break_category_params
-      params.require(:garage_break_category).permit(:destroys)
+      params.require(:garage_break_category).permit(:garage_id, :break_category_id)
     end
   end
 end
