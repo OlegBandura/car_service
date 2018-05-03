04456570c394d8df10987bc631437712c5d9441f
Unknown <Bomber.qw@gmail.com>
2018-05-03 14:29:56 +0300

    Add index to join table

---
+++ db/migrate/20180503112545_add_index_to_two_fields.rb
@@ -0,0 +1,6 @@
+class AddIndexToTwoFields < ActiveRecord::Migration[5.1]
+  def change
+    add_index :joins_garages_break_categories, ["garage_id", "break_category_id"],
+      :unique => true, :name => 'garage_index'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180503110713) do
+ActiveRecord::Schema.define(version: 20180503112545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 20180503110713) do
   create_table "joins_garages_break_categories", id: false, force: :cascade do |t|
     t.integer "garage_id"
     t.integer "break_category_id"
+    t.index ["garage_id", "break_category_id"], name: "garage_index", unique: true
   end
 
   create_table "orders", force: :cascade do |t|
