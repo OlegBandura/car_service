af01a66bce5162c47f88fe1d01af85fbdcfdca61
Unknown <Bomber.qw@gmail.com>
2018-05-10 17:08:04 +0300

    Show break categories & break_sub_categories in garage page

--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -4,31 +4,29 @@ class GaragesController < ApplicationController
     @garage = Garage.find(params[:id])
     @director = User.find(@garage.user_id)
 
-    @cars = Hash.new { |hsh, key| hsh[key] = [] }
-
-    break_categories = JoinsGaragesBreakCategory.select_added_category(@garage.id)
+    # @categories = Hash.new { |hsh, key| hsh[key] = [] }
+    @s_categories = []
+    @categories = []
+     break_categories = JoinsGaragesBreakCategory.select_added_category(@garage.id)
 
     break_categories.each do |b|
       @category_id = b.break_category_id
-      @category = b.break_category_name
-      puts '____________________'
-      puts @category_id
-      puts b.break_category_name
-    end
+      @categories.push(b.break_category_name)
+
+
+
 
     @break_sub_category = BreakCategory.joins(:break_sub_categories).select(
     'break_sub_categories.break_sub_category_name, break_sub_categories.id,
     break_categories.break_category_name,
     break_sub_categories.break_category_id').where(
     'break_sub_categories.break_category_id = ?', @category_id
-   )
+    )
 
     @break_sub_category.each do |c|
-
-      @cars[c[:break_category_name]].push(c)
-
+      @s_categories.push(c)
     end
-
+  end
     @comment = GarageComment.comment(@garage.id)
   end
 end
