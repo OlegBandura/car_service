dca3955bff21dea36545508aaa65091f50b2b4af
Unknown <Bomber.qw@gmail.com>
2018-05-10 17:15:40 +0300

    add scope to break_category

--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -4,24 +4,17 @@ class GaragesController < ApplicationController
     @garage = Garage.find(params[:id])
     @director = User.find(@garage.user_id)
 
-    # @categories = Hash.new { |hsh, key| hsh[key] = [] }
     @s_categories = []
     @categories = []
-     break_categories = JoinsGaragesBreakCategory.select_added_category(@garage.id)
+    break_categories = JoinsGaragesBreakCategory.select_added_category(
+      @garage.id
+    )
 
     break_categories.each do |b|
       @category_id = b.break_category_id
       @categories.push(b.break_category_name)
 
-
-
-
-    @break_sub_category = BreakCategory.joins(:break_sub_categories).select(
-    'break_sub_categories.break_sub_category_name, break_sub_categories.id,
-    break_categories.break_category_name,
-    break_sub_categories.break_category_id').where(
-    'break_sub_categories.break_category_id = ?', @category_id
-    )
+    @break_sub_category = BreakCategory.get_category_properties(@category_id)
 
     @break_sub_category.each do |c|
       @s_categories.push(c)
--- app/models/break_category.rb
+++ app/models/break_category.rb
@@ -3,10 +3,6 @@ class BreakCategory < ApplicationRecord
   has_many :break_sub_categories, dependent: :destroy
   has_many :joins_garages_break_categories
 
-  # scope :break_sub_category, -> { joins(:break_sub_categories).select(
-  #       'break_categories.name, break_sub_categories.break_sub_category_name,
-  #       break_sub_categories.id') }
-
   scope :select_category_sub_category, -> {
     left_joins(:break_sub_categories).select('break_categories.break_category_name,
     break_sub_categories.break_sub_category_name,
@@ -14,5 +10,13 @@ class BreakCategory < ApplicationRecord
     break_sub_categories.break_category_id')
   }
   scope :select_category, -> { order(break_category_name: :asc) }
+
+  scope :get_category_properties, -> (category_id) {
+    joins(:break_sub_categories).select(
+      'break_sub_categories.break_sub_category_name, break_sub_categories.id,
+      break_categories.break_category_name,
+      break_sub_categories.break_category_id').where(
+      'break_sub_categories.break_category_id = ?', category_id
+    ) }
 
 end
