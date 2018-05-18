13d9417c64a0e1137348e1b330b283e8faf22618
Unknown <Bomber.qw@gmail.com>
2018-05-18 12:49:20 +0300

    add hash categories

--- app/controllers/admin/brands_controller.rb
+++ app/controllers/admin/brands_controller.rb
@@ -1,41 +1,43 @@
-class Admin::BrandsController < Admin::BaseController
-  # load_and_authorize_resource
-  def index
-    cars = Brand.get_car
+module Admin
+  class BrandsController < BaseController
+    # load_and_authorize_resource
+    def index
+      cars = Brand.get_car
 
-    @cars = Hash.new { |hsh, key| hsh[key] = [] }
-    cars.each do |car|
-      @cars[car[:brand_name]].push(car)
+      @cars = Hash.new { |hsh, key| hsh[key] = [] }
+      cars.each do |car|
+        @cars[car[:brand_name]].push(car)
+      end
     end
-  end
 
-  def create
-    @brand = Brand.create(brand_params)
-    redirect_to admin_brands_path
-  end
-
-  def edit
-    @brand = Brand.find(params[:id])
-  end
-
-  def update
-    @brand = Brand.find(params[:id])
-    if @brand.update(brand_params)
+    def create
+      @brand = Brand.create(brand_params)
       redirect_to admin_brands_path
-    else
-      render action 'edit'
     end
-  end
 
-  def destroy
-    brand = Brand.find(params[:id])
-    brand.destroy
-    redirect_to admin_brands_path
-  end
+    def edit
+      @brand = Brand.find(params[:id])
+    end
 
-  private
+    def update
+      @brand = Brand.find(params[:id])
+      if @brand.update(brand_params)
+        redirect_to admin_brands_path
+      else
+        render action 'edit'
+      end
+    end
 
-  def brand_params
-    params.require(:brand).permit(:brand_name)
+    def destroy
+      brand = Brand.find(params[:id])
+      brand.destroy
+      redirect_to admin_brands_path
+    end
+
+    private
+
+    def brand_params
+      params.require(:brand).permit(:brand_name)
+    end
   end
 end
